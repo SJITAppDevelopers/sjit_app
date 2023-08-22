c00e7bd2069a8e8fe8c70405161d7809cad0f202
Vigram <vigramkm@gmail.com>
2023-08-22 09:06:23 +0530

    created the login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sjit_app/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
+      home: const LoginScreen(),
     );
   }
 }
