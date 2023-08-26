c7242f68e0ae829379815e10e24c0d95090f3cc5
vitasos <sanjithkumarm2003@gmail.com>
2023-08-26 22:13:34 +0530

    sanjith

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sjit_app/screens/home_screen.dart';
+import 'package:sjit_app/screens/login_screen.dart';
 // import 'package:sjit_app/screens/login_screen.dart';
 // import 'package:sjit_app/screens/profile_screen.dart';
 
@@ -14,8 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      // home: LoginScreen(),
-      home: HomeScreen(),
+      home: LoginScreen(),
+      //home: HomeScreen(),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -24,143 +24,145 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false, // set it to false
       body: SafeArea(
         child: Container(
           color: const Color(0xFF121536),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft:
-                          Radius.circular(30), // Adjust the radius as needed
-                      bottomRight:
-                          Radius.circular(30), // Adjust the radius as needed
-                    ),
-                  ),
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        height: 60,
-                      ),
-                      Image.asset(
-                        "images/clgLogo.png",
-                        width: 175,
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Image.asset(
-                        "images/clgName.png",
-                        width: 275,
-                      ),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      Image.asset(
-                        "images/autoName.png",
-                        width: 150,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  "SIGN IN",
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: TextField(
-                    controller: _rollNo,
-                    style: const TextStyle(color: Colors.black),
-                    decoration: const InputDecoration(
-                      hintText: 'Roll No.',
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintStyle: TextStyle(color: Colors.black),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors
-                                .black), // Color of border when not focused
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color:
-                                Colors.black), // Color of border when focused
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft:
+                            Radius.circular(30), // Adjust the radius as needed
+                        bottomRight:
+                            Radius.circular(30), // Adjust the radius as needed
                       ),
                     ),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Image.asset(
+                          "images/clgLogo.png",
+                          width: 160,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Image.asset(
+                          "images/clgName.png",
+                          width: 275,
+                        ),
+                        const SizedBox(
+                          height: 3,
+                        ),
+                        Image.asset(
+                          "images/autoName.png",
+                          width: 150,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: TextField(
-                    controller: _password,
-                    style: const TextStyle(color: Colors.black),
-                    decoration: const InputDecoration(
-                      hintText: 'Password',
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintStyle: TextStyle(color: Colors.black),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors
-                                .black), // Color of border when not focused
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color:
-                                Colors.black), // Color of border when focused
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    "SIGN IN",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                    child: TextField(
+                      controller: _rollNo,
+                      style: const TextStyle(color: Colors.black),
+                      decoration: const InputDecoration(
+                        hintText: 'Roll No.',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintStyle: TextStyle(color: Colors.black),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors
+                                  .black), // Color of border when not focused
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color:
+                                  Colors.black), // Color of border when focused
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Colors.white), // White background color
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        Colors.black), // White text color
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 10),
+                    child: TextField(
+                      controller: _password,
+                      style: const TextStyle(color: Colors.black),
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintStyle: TextStyle(color: Colors.black),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors
+                                  .black), // Color of border when not focused
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color:
+                                  Colors.black), // Color of border when focused
+                        ),
+                      ),
+                    ),
                   ),
-                  onPressed: () {
-                    // Access the username and password here
-                    String username = _rollNo.text;
-                    String password = _password.text;
-
-                    // Now you can use the username and password variables as needed
-                    print('Username: $username');
-                    print('Password: $password');
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const HomeScreen()),
-                    );
-                  },
-                  child: const Text('Submit'),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Expanded(
-                  child: Container(
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.white), // White background color
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Colors.black), // White text color
+                    ),
+                    onPressed: () {
+                      // Access the username and password here
+                      String username = _rollNo.text;
+                      String password = _password.text;
+          
+                      // Now you can use the username and password variables as needed
+                      print('Username: $username');
+                      print('Password: $password');
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
+                      );
+                    },
+                    child: const Text('Submit'),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
                     decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
@@ -185,8 +187,8 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
